d3bd1bb0d47bb90d7fec701e02c8dc6335f9f8d5
Rakshit Agrawal <rakshit_agrawal@outlook.com>
2015-06-09 14:47:30 -0700

    Pickles, all data load and value attributed

--- src/data_fetch.py
+++ src/data_fetch.py
@@ -1,5 +1,9 @@
 import json
+import os
+from string import lower
+from datetime import datetime
 import numpy as np
+import pickle
 
 __author__ = 'rakshit'
 
@@ -15,15 +19,69 @@ class DataFetch:
         self.post_list = set()
         self.blog_list = set()
         self.author_list = set()
+        self.tag_list = set()
 
         self.user_author = {}
         self.user_blog = {}
         self.user_post = {}
+        self.user_tags = {}
 
-    def read_file(self, filename):
+    def check_pickles(self):
+        # Check for existence of pickles
+        pass
+    
+    def load_data(self, datafile):
+        if datafile == "user_author.p":
+            if os.path.isfile("user_author.p"):
+                print("loading <user_author> from datastore")
+                user_author = pickle.load(open("user_author.p", "rb"))
+                print("data loaded")
+                return user_author
+            else:
+                print("generating <user_author>")
+                self.read_file(POST_DATA_FILE,type="author")
+                print("dumping to data store")
+                pickle.dump(self.user_author, open("user_author.p", "wb"))
+                print("data dumped")
+                return self.user_author
+
+        elif datafile == "user_blog.p":
+            if os.path.isfile("user_blog.p"):
+                print("loading <user_blog> from datastore")
+                user_blog = pickle.load(open("user_blog.p", "rb"))
+                print("data loaded")
+                return user_blog
+            else:
+                print("generating <user_blog>")
+                self.read_file(POST_DATA_FILE,type="blog")
+                print("dumping to data store")
+                pickle.dump(self.user_blog, open("user_blog.p", "wb"))
+                print("data dumped")
+                return self.user_blog
+
+        elif datafile == "user_tags.p":
+            if os.path.isfile("user_tags.p"):
+                print("loading <user_tags> from datastore")
+                user_tags = pickle.load(open("user_tags.p", "rb"))
+                print("data loaded")
+                return user_tags
+            else:
+                print("generating <user_tags>")
+                self.read_file(POST_DATA_FILE,type="tags")
+                print("dumping to data store")
+                pickle.dump(self.user_tags, open("user_tags.p", "wb"))
+                print("data dumped")
+                return self.user_tags
+
+
+    def read_file(self, filename, type):
         f = open(filename, 'r')
         strf = f.read()
         strarr = strf.split('\n')
+        #self.file_parse(strarr)
+
+
+    #def file_parse(self, strarr):
 
         for i,v in enumerate(strarr):
             # Extract elements from json here.
@@ -35,38 +93,57 @@ class DataFetch:
             post_id = v["post_id"]
             blog_id = v["blog"]
             author = v["author"]
+            tags = v["tags"]
 
             self.post_list.add(post_id)
             self.blog_list.add(blog_id)
             self.author_list.add(author)
 
+            for i in tags:
+                tag = lower(i)
+                self.tag_list.add(tag)
+
             users = v["likes"]
             for el in users:
                 uid = el["uid"]
 
-                if(self.user_post.has_key(uid)):
-                    self.user_post[uid].append(dict(post=post_id, likes = 1))
-                else:
-                    self.user_post[uid] = []
-                    self.user_post[uid].append(dict(post=post_id, likes = 1))
+                if type=="blog":
 
+                    if(self.user_blog.has_key(uid)):
+                        if blog_id in self.user_blog[uid]:
+                            self.user_blog[uid][blog_id]+=1
+                        else:
+                            self.user_blog[uid][blog_id] = 1
+                    else:
+                        self.user_blog[uid] = {}
+                        self.user_blog[uid][blog_id] = 1
+                        
+                elif type=="author":
 
+                    if(self.user_author.has_key(uid)):
+                        if author in self.user_author[uid]:
+                            self.user_author[uid][author]+=1
+                        else:
+                            self.user_author[uid][author] = 1
+                    else:
+                        self.user_author[uid] = {}
+                        self.user_author[uid][author] = 1
 
-                if(self.user_blog.has_key(uid)):
-                    if blog_id in self.user_blog[uid]:
-                        self.user_blog[uid][blog_id]
-                        self.user_blog[uid].append({blog_id:})
-                else:
-                    self.user_blog[uid] = []
-                    self.user_blog[uid].append(dict(blog=blog_id, likes =1))
+                elif type=="tags":
 
+                    if(self.user_tags.has_key(uid)):
+                        for tag in tags:
+                            tag = lower(tag)
+                            if tag in self.user_tags[uid]:
+                                self.user_tags[uid][tag]+=1
+                            else:
+                                self.user_tags[uid][tag] = 1
+                    else:
+                        for tag in tags:
+                            tag = lower(tag)
+                            self.user_tags[uid] = {}
+                            self.user_tags[uid][tag] = 1
 
-
-                if(self.user_author.has_key(uid)):
-                    self.user_author[uid].append(dict(author=author, likes = 1))
-                else:
-                    self.user_author[uid] = []
-                    self.user_author[uid].append(dict(author=author, likes = 1))
 
 
     def mat_initiate(self, column_list):
@@ -76,37 +153,9 @@ class DataFetch:
 
         self.ymat = np.zeros((dimx, dimy))
 
-    def mat_update(self):
-        # Update values in matrix
-        f = open(filename, 'r')
-        strf = f.read()
-        strarr = strf.split('\n')
-
-        for i,v in enumerate(strarr):
-            # Extract elements from json here.
-            try:
-                v = json.loads(v)
-            except:
-                continue
-            #print v
-            post_id = v["post_id"]
-            blog_id = v["blog"]
-            author = v["author"]
-
-            self.post_list.add(post_id)
-            self.blog_list.add(blog_id)
-            self.author_list.add(author)
-
-            users = v["likes"]
-            for el in users:
-                uid = el["uid"]
-                self.user_list.add(uid)
-
-
-        pass
 
     def print_lists(self):
-        for k,v in self.user_post.iteritems():
+        for k,v in self.user_blog.iteritems():
             print unicode(k) + "--" + unicode(v)
 
         print len(self.author_list)
@@ -116,8 +165,15 @@ class DataFetch:
 
 
 if __name__ == "__main__":
-    USER_DATA_FILE = "../data/sample011.json" # 5000 lines from trainPosts
+    POST_DATA_FILE = "../data/sample011.json" # 5000 lines from trainPosts
 
     c = DataFetch()
-    c.read_file(USER_DATA_FILE)
-    c.print_lists()
+    #c.read_file(POST_DATA_FILE)
+    t1 = datetime.now()
+    data_dict = c.load_data("user_tags.p")
+    t2 = datetime.now()
+    for k,v in data_dict.iteritems():
+        print k
+        print v
+    print "time taken" + str(t2-t1)
+    #c.print_lists()
